e3bad7258f558a5a9e2c902d932c36d1506dfaa4
Samuel Adeyemi <samueladeyemi2006@gmail.com>
2022-04-24 22:36:13 +0100

    updated server.js

--- server.js
+++ server.js
@@ -3,14 +3,7 @@ fs = require("fs");
 
 http
   .createServer(function (req, res) {
-    fs.readFile(index.html + req.url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end(json.stringify(err));
-        return;
-      }
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Hello Nodejs");
-    });
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello Nodejs");
   })
   .listen(8080);
