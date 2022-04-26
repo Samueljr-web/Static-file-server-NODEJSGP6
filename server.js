77877575590b4ca555b47b0d9064e141da4d5776
Samuel Adeyemi <samueladeyemi2006@gmail.com>
2022-04-26 18:27:38 +0100

    code cleanup

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 var http = require("http");
-fs = require("fs");
+var fs = require("fs");
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello Nodejs");
-  })
-  .listen(8080);
+const host = "127.0.0.1";
+const port = 8080;
+
+const server = http.createServer(function (req, res) {
+  const urlPath = req.url;
+
+  if (urlPath === "/") {
+    fs.readFile("./pages/index.html", (err, content) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content, "utf-8");
+    });
+  } else if (urlPath === "/about") {
+    fs.readFile("./pages/about.html", (err, content) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content, "utf-8");
+    });
+  } else if (urlPath === "/sys") {
+    fs.readFile("osinfo.json", "samuel", (err, content) => {
+      res.writeHead(201, { "Content-Type": "text/plain" });
+      res.end("Your OS info has been saved successfully!");
+    });
+  } else {
+    fs.readFile("./pages/404.html", (err, content) => {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end(content, "utf-8");
+    });
+  }
+});
+server.listen(port, host, () => {
+  console.log(`Your Server is Running at ${host}:${port}`);
+});
